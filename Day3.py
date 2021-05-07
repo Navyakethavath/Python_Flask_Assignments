bf9ed22ebb093565b97d596fde9a2c37fcf2c504
Navya Kethavath <83653041+Navyakethavath@users.noreply.github.com>
2021-05-07 20:10:08 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,17 @@
+#AGE CALCULATOR
+x=int(input("enter the year of the birth of the user:"))
+y=2021
+age=y-x
+print(age)
+
+
+#Simple Calculator:
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("a + b = {}".format(a + b))
+print("a - b = {}".format(a - b))
+print("a * b = {}".format(a * b))
+print("a / b = {}".format(a / b))
+print("a % b = {}".format(a % b))
+print("a // b = {}".format(a // b))
+print("a * b = {}".format(a * b))
